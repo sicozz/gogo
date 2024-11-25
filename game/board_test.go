0a220e1a67ac31c1999dd9952b784f5bd812c888
sicoz <simozuluaga@gmail.com>
2024-11-25 06:50:39 -0500

    test(game): use BoardBuilder in tests setup

--- game/board_test.go
+++ game/board_test.go
@@ -6,8 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPositions(t *testing.T) {
-	b := NewBoard(BoardSize19)
+func TestBoardBuilder(t *testing.T) {
 	g := GovBlack
 	poss := positions{
 		NewPosition(0, 0),
@@ -15,10 +14,12 @@ func TestPositions(t *testing.T) {
 		NewPosition(1, 0),
 		NewPosition(1, 1),
 	}
-	b = b.seize(poss[0], g)
-	b = b.seize(poss[1], g)
-	b = b.seize(poss[2], g)
-	b = b.seize(poss[3], g)
+    b := NewBoardBuilder(BoardSize19).
+        SetPosition(poss[0], g).
+        SetPosition(poss[1], g).
+        SetPosition(poss[2], g).
+        SetPosition(poss[3], g).
+        Build()
 
 	resPositions := Positions(b, GovBlack)
 
@@ -41,15 +42,11 @@ func TestClaimPosition_NotClaimedPosition(t *testing.T) {
 
 // Test the position claiming for a claimed position.
 func TestClaimPosition_ClaimedPosition(t *testing.T) {
-	b := NewBoard(BoardSize9)
 	p := NewPosition(0, 1)
-	bB, err := ClaimPosition(b, p, GovBlack)
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, GovBlack, bB.governance(p), "Position should have had black-governance")
+	b := NewBoardBuilder(BoardSize9).SetPosition(p, GovBlack).Build()
+	assert.Equal(t, GovBlack, b.governance(p), "Position should have had black-governance")
 
-	_, err = ClaimPosition(bB, p, GovWhite)
+    _, err := ClaimPosition(b, p, GovWhite)
 	assert.EqualError(t, err, ErrPositionTaken.Error(), "Failed prevention of claimed position claim")
 }
 
@@ -62,33 +59,32 @@ func TestClaimPosition_NeutralGovernance(t *testing.T) {
 }
 
 func TestIsAlive_Atari(t *testing.T) {
-    b := NewBoard(BoardSize11)
-    pSBlack := positions{
-        { x: 0, y: 0 },
-        { x: 0, y: 1 },
-        { x: 0, y: 2 },
-        { x: 0, y: 3 },
-    }
-    pSWhite := positions{
-        { x: 1, y: 0 },
-        { x: 1, y: 1 },
-        { x: 1, y: 2 },
-        { x: 1, y: 3 },
-    }
-    for _, p := range pSBlack {
-        b = b.seize(p, GovBlack)
-    }
-    for _, p := range pSWhite {
-        b = b.seize(p, GovWhite)
-    }
+    pS := []struct{
+        pos position
+        gov governance
+    }{
+        { pos: position{ x: 0, y: 0 }, gov: GovBlack },
+        { pos: position{ x: 0, y: 1 }, gov: GovBlack },
+        { pos: position{ x: 0, y: 2 }, gov: GovBlack },
+        { pos: position{ x: 0, y: 3 }, gov: GovBlack },
 
-    for _, p := range append(pSBlack, pSWhite...) {
-        assert.True(t, IsAlive(b, p), "Failed to tell position life status")
+        { pos: position{ x: 1, y: 0 }, gov: GovWhite },
+        { pos: position{ x: 1, y: 1 }, gov: GovWhite },
+        { pos: position{ x: 1, y: 2 }, gov: GovWhite },
+        { pos: position{ x: 1, y: 3 }, gov: GovWhite },
+    }
+    builder := NewBoardBuilder(BoardSize11)
+    for _, p := range pS {
+        builder.SetPosition(p.pos, p.gov)
+    }
+    b := builder.Build()
+
+    for _, p := range pS {
+        assert.True(t, IsAlive(b, p.pos), "Failed to tell position life status")
     }
 }
 
 func TestIsAlive_Capture(t *testing.T) {
-    b := NewBoard(BoardSize11)
     pSBlack := positions{
         { x: 0, y: 0 },
         { x: 0, y: 1 },
@@ -102,12 +98,14 @@ func TestIsAlive_Capture(t *testing.T) {
         { x: 1, y: 3 },
         { x: 0, y: 4 },
     }
+    builder := NewBoardBuilder(BoardSize11)
     for _, p := range pSBlack {
-        b = b.seize(p, GovBlack)
+        builder.SetPosition(p, GovBlack)
     }
     for _, p := range pSWhite {
-        b = b.seize(p, GovWhite)
+        builder.SetPosition(p, GovWhite)
     }
+    b := builder.Build()
 
     for _, p := range pSBlack {
         assert.False(t, IsAlive(b, p), "Failed to tell position life status")
@@ -118,7 +116,6 @@ func TestIsAlive_Capture(t *testing.T) {
 }
 
 func TestIsAlive_Case00(t *testing.T) {
-    b := NewBoard(BoardSize11)
     pSBlack := positions{
         { x: 0, y: 1 },
         { x: 1, y: 0 },
@@ -126,10 +123,12 @@ func TestIsAlive_Case00(t *testing.T) {
         { x: 2, y: 1 },
     }
     pWhite := position{ x: 1, y: 1 }
+    builder := NewBoardBuilder(BoardSize11)
     for _, p := range pSBlack {
-        b = b.seize(p, GovBlack)
+        builder.SetPosition(p, GovBlack)
     }
-    b = b.seize(pWhite, GovWhite)
+    builder.SetPosition(pWhite, GovWhite)
+    b := builder.Build()
 
     assert.False(t, IsAlive(b, pWhite), "Failed to tell position life status")
     for _, p := range pSBlack {
